05368e010baa9f231a6350389bfb6e1035774d2f
Yogesh Chandra Tripathi <137177000+yogeshchandratripathi@users.noreply.github.com>
2024-01-01 15:51:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
